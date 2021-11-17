f9c4664bbc96b00cf619c3e6a60f92aa39caa3e1
katar <>
2021-11-17 21:14:33 +0100

    babel, prettier, ts, webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,46 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+var PrettierPlugin = require("prettier-webpack-plugin");
+
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
     devServer: {
         contentBase: './dist',
         port: 3000
     },
-    plugins: [new HtmlWebpackPlugin()]
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react',
+                            '@babel/preset-typescript',
+                        ],
+                    },
+                },
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './src/index.html' }),
+        new PrettierPlugin()
+
+    ]
 };
